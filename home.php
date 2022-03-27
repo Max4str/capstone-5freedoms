36a504358724bd3cc50fdcae73529c6d9f65ef5b
Max <max4str@gmail.com>
2022-03-27 17:32:51 -0600

    .

--- home.php
+++ home.php
@@ -18,6 +18,30 @@
 
 	<main id="primary" class="site-main">
 
+		<!-- Banner -->
+		<div class="banner">
+			<?php $banner = get_field('banner'); ?>
+			<?php if($banner): ?>
+				
+				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				<!-- Banner Image -->
+				<?php $banner_image = $banner['banner_image']; ?>
+				<?php if($banner_image): ?>
+					<?php 
+						$img_url = $banner_image['url'];
+					?>
+					<img src="<?php print_r(esc_url($img_url)); ?>">
+				<?php endif; ?>
+				
+				<!-- Banner Text -->
+				<?php $banner_text = $banner['banner_text']; ?>
+				<?php if($banner_text): ?>
+					<p><?php _e($banner_text); ?></p>
+				<?php endif; ?>
+				
+			<?php endif; ?>
+		</div>
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
